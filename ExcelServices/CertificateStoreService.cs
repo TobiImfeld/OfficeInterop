7e06bcc37e3ef86624631443afb657c7a2903d0a
tobiasImfeld <tobias_imfeld@hotmail.com>
2021-06-05 13:51:27 +0200

    Remove unused code

--- ExcelServices/CertificateStoreService.cs
+++ ExcelServices/CertificateStoreService.cs
@@ -16,13 +16,9 @@ namespace ExcelServices
             {
                 store.Open(OpenFlags.ReadOnly);
 
-                // Place all certificates in an X509Certificate2Collection object.
                 X509Certificate2Collection certCollection = store.Certificates;
-                // If using a certificate with a trusted root you do not need to FindByTimeValid, instead:
-                // currentCerts.Find(X509FindType.FindBySubjectDistinguishedName, certName, true);
-                //X509Certificate2Collection currentCerts = certCollection.Find(X509FindType.FindByTimeValid, DateTime.Now, false);
-                X509Certificate2Collection currentCerts = certCollection.Find(X509FindType.FindByIssuerName, "TobiOfficeCert", false);
-                X509Certificate2Collection signingCerts = currentCerts.Find(X509FindType.FindByIssuerName, "TobiOfficeCert", false);
+                X509Certificate2Collection currentCerts = certCollection.Find(X509FindType.FindByIssuerName, certName, false);
+                X509Certificate2Collection signingCerts = currentCerts.Find(X509FindType.FindByIssuerName, certName, false);
 
                 if (signingCerts.Count == 0)
                 {
