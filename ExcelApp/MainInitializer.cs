26d23d754e28266e3e248edb1d459523a2b974bf
tobiasImfeld <tobias_imfeld@hotmail.com>
2021-06-06 19:33:44 +0200

    Introduce MainInitializer to start all services

---
+++ ExcelApp/MainInitializer.cs
@@ -0,0 +1,55 @@
+﻿using ExcelServices;
+using Logging;
+using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
+
+namespace ExcelApp
+{
+    public class MainInitializer
+    {
+        private ILoggerFactory loggerFactory;
+        private ICertificateStoreService certificateStoreService;
+        private IExcelService excelService;
+
+        private MainInitializer() { }
+
+        public static MainInitializer Init()
+        {
+            var mainInitalizier = new MainInitializer();
+            mainInitalizier.InitServices();
+
+            return mainInitalizier;
+        }
+
+        public ILoggerFactory GetLoggerFactory()
+        {
+            return this.loggerFactory;
+        }
+
+        public ICertificateStoreService GetCertificateService()
+        {
+            return this.certificateStoreService;
+        }
+
+        public IExcelService GetExcelService()
+        {
+            return this.excelService;
+        }
+
+        private void InitServices()
+        {
+            var logFile = ConfigurationManager.AppSettings["LogFilePath"];
+            LoggerComponent.InitLogger(logFile);
+
+            var serviceProvider = new ServiceCollection()
+                .AddSingleton<ILoggerFactory, LoggerFactory>()
+                .AddTransient<ICertificateStoreService, CertificateStoreService>()
+                .AddTransient<IExcelService, ExcelService>()
+                .BuildServiceProvider();
+
+            this.loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+            this.certificateStoreService = serviceProvider.GetService<ICertificateStoreService>();
+            this.excelService = serviceProvider.GetService<IExcelService>();
+        }
+    }
+}
--- ExcelApp/Program.cs
+++ ExcelApp/Program.cs
@@ -1,27 +1,20 @@
-﻿using System.Configuration;
-using ExcelServices;
-using CommandLine;
+﻿using CommandLine;
 using Logging;
 
 namespace ExcelApp
 {
     public class Program
     {
-        private static readonly ILoggerFactory loggerFactory = new LoggerFactory();
-
         static void Main(string[] args)
         {
-            var logFile = ConfigurationManager.AppSettings["LogFilePath"];
-            LoggerComponent.InitLogger(logFile);
+            var main = MainInitializer.Init();
 
-            ILogger logger = loggerFactory.Create<Program>();
+            var logger = main.GetLoggerFactory().Create<Program>();
 
             logger.Info("Start ExcelApp");
 
-            ICertificateStoreService certificateStoreService = new CertificateStoreService(loggerFactory);
-            IExcelService excelService = new ExcelService(loggerFactory, certificateStoreService);
-            excelService.SetPathToFiles(@"C:\Temp\");
-            excelService.AddDigitalSignature("TobiOfficeCert");
+            main.GetExcelService().SetPathToFiles(@"C:\Temp\");
+            main.GetExcelService().AddDigitalSignature("TobiOfficeCert");
 
             logger.Info("Stop ExcelApp");
             LoggerComponent.Close();
