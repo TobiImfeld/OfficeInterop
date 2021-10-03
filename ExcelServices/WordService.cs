cd6a84cf9a4e6cf1679d80c257edf8d567367000
tobiasImfeld <tobias_imfeld@hotmail.com>
2021-10-03 18:34:57 +0200

    Add contstructor to WordService

--- ExcelServices/WordService.cs
+++ ExcelServices/WordService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,20 @@ namespace ExcelServices
 {
     public class WordService : IWordService
     {
+        private readonly ILogger logger;
+        private readonly ICertificateStoreService certificateStoreService;
+        private readonly IFileService fileService;
+
+        public WordService(
+            ILoggerFactory loggerFactory,
+            ICertificateStoreService certificateStoreService,
+            IFileService fileService)
+        {
+            this.logger = loggerFactory.Create<ExcelVbaService>();
+            this.certificateStoreService = certificateStoreService;
+            this.fileService = fileService;
+        }
+
+
     }
 }
