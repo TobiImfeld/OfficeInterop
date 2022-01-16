516b77c5fbce15d0f0fe892a79e5ded16d7405a2
tobiasImfeld <tobias_imfeld@hotmail.com>
2022-01-16 23:16:24 +0100

    Introduce ParserService unit test

---
+++ TestCommandLineParser/TestParserService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestCommandLineParser
+{
+    [TestClass]
+    public class TestParserService
+    {
+        [TestMethod]
+        public void IsInputCorrect()
+        {
+            var test = new TestingBase();
+            var parserService = test
+                .InitServices()
+                .GetParserService();
+
+            var input = "F:\\Firma\\Firma\\SERVICE\\Anlagen\\05_GBK\\GBK Vercorin - Sigeroulaz - Crêt du Midi, Vercorin\\GBK Vercorin - Sigeroulaz - Crêt du Midi, Vercorin.xlsm";
+
+            Assert.AreEqual(0, parserService.ParseInput(input));
+        }
+    }
+}
---
+++ TestCommandLineParser/TestingBase.cs
@@ -0,0 +1,31 @@
+﻿using CommandLineParser;
+using ExcelServices;
+using Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TestCommandLineParser
+{
+    public class TestingBase
+    {
+        private ServiceProvider serviceProvider;
+
+        public TestingBase InitServices()
+        {
+            this.serviceProvider = new ServiceCollection()
+                .AddSingleton<ILoggerFactory, LoggerFactory>()
+                .AddTransient<ICertificateStoreService, CertificateStoreService>() //service mocken! kein echter zugriff auf certificate store!
+                .AddTransient<IExcelService, ExcelService>()
+                .AddTransient<IParserService, ParserService>()
+                .AddTransient<IFileService, FileService>()
+                .AddTransient<IExcelVbaService, ExcelVbaService>()
+                .BuildServiceProvider();
+
+            return this;       
+        }
+
+        public IParserService GetParserService()
+        {
+            return this.serviceProvider.GetService<IParserService>();
+        }
+    }
+}
