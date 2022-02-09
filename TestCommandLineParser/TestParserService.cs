8d76b34a87ea14e8cdb6e369fcdb35e4363a4df6
tobiasImfeld <tobias_imfeld@hotmail.com>
2022-02-09 16:10:08 +0100

    Change test to IllegalCharacterInPath to test it

--- TestCommandLineParser/TestParserService.cs
+++ TestCommandLineParser/TestParserService.cs
@@ -7,14 +7,14 @@ namespace TestCommandLineParser
     public class TestParserService
     {
         [TestMethod]
-        public void IsInputCorrect()
+        public void IllegalCharacterInPath_OneCommandAndPathOptionWithPathValue()
         {
             var test = new TestingBase();
             var parserService = test
                 .InitServices()
                 .GetParserService();
 
-            var input = "F:\\Firma\\Firma\\SERVICE\\Anlagen\\05_GBK\\GBK Vercorin - Sigeroulaz - Crêt du Midi, Vercorin\\GBK Vercorin - Sigeroulaz - Crêt du Midi, Vercorin.xlsm";
+            var input = "delsigfromvba -f \"C:\\Users\\sasc\\Desktop\\ICAD_Cbl_material_DRIVE_GAA0014631_FAG.xlsm\"";
 
             Assert.AreEqual(0, parserService.ParseInput(input));
         }
